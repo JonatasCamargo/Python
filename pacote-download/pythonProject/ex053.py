8e3fd20d8ea8df02da19bbe75c5f58e4d2c8bad2
Jonatas Camargo <jonatascamargo28@gmail.com>
2023-09-18 19:27:13 -0300

    new solution

---
+++ pacote-download/pythonProject/ex053.py
@@ -0,0 +1,22 @@
+#captura de dados
+frase = str(input('Digite uma frase: ')).strip().upper()
+
+#cortes por espaços (separação)
+palavras = frase.split()
+
+#agrupamento de caracteres
+junto = ''.join(palavras)
+
+#inversão da frase digitada
+inverso = ''
+
+#Iteração para ler os carcateres de trá para frente
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso da {junto} é {inverso}')    
+
+#condição se é palíndromo ou não
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palídromo')    
